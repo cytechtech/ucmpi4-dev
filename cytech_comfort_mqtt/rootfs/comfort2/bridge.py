175fc8ffffa845ab75927af7a0500309ad0f2c35
ghall-zebsys <56168552+ghall-zebsys@users.noreply.github.com>
2026-09-01 15:11:22 +0100

    Test

--- cytech_comfort_mqtt/rootfs/comfort2/bridge.py
+++ cytech_comfort_mqtt/rootfs/comfort2/bridge.py
@@ -236,11 +236,12 @@ try:
     )
 
     if mosquitto_login_changed:
-        logger.info(
-            "Mosquitto login configuration changed; "
-            "restarting Mosquitto"
-        )
-        restart_mosquitto()
+        mark_mosquitto_restart_required()
+
+    # TEMPORARY TEST
+    mark_mosquitto_restart_required()
+
+    restart_mosquitto_if_required()
 
 except Exception:
     logger.exception(
@@ -3511,56 +3512,33 @@ def main():
     global mqttc
     MQTT_VERSION = mqtt.MQTTv5
 
-    # if settings.MQTT_TLS_ENABLED:
-    #     logger.info("Checking MQTT certificate installation")
+    if settings.MQTT_TLS_ENABLED:
+        logger.info("Checking MQTT certificate installation")
 
-    #     try:
-    #         ensure_certificate_set()
+        try:
+            ensure_certificate_set()
 
-    #         logger.info("Checking MQTT TLS deployment")
+            logger.info("Checking MQTT TLS deployment")
 
-    #         mosquitto_tls_changed = deploy_mosquitto_tls_files(
-    #             require_client_certificate=settings.MQTT_MUTUAL_TLS,
-    #         )
+            mosquitto_tls_changed = deploy_mosquitto_tls_files(
+                require_client_certificate=settings.MQTT_MUTUAL_TLS,
+            )
 
-    #         if mosquitto_tls_changed:
-    #             mark_mosquitto_restart_required()
-    #         mark_mosquitto_restart_required()
-    #         restart_mosquitto_if_required()
+            if mosquitto_tls_changed:
+                mark_mosquitto_restart_required()
 
-    #     except Exception:
-    #         logger.exception("MQTT certificate setup failed")
-    #         raise
+            restart_mosquitto_if_required()
 
-    # else:
-    #     logger.info(
-    #         "MQTT password mode selected - TLS certificate setup not required"
-    #     )
+        except Exception:
+            logger.exception("MQTT certificate setup failed")
+            raise
 
-    try:
-        mosquitto_login_changed = ensure_managed_login(
-            settings.MQTTUSERNAME,
-            settings.MQTTPASSWORD,
-        )
-
+    else:
         logger.info(
-            "Mosquitto login configuration changed: %s",
-            mosquitto_login_changed,
+            "MQTT password mode selected - TLS certificate setup not required"
         )
 
-        if mosquitto_login_changed:
-            mark_mosquitto_restart_required()
 
-        # TEMPORARY TEST
-        mark_mosquitto_restart_required()
-
-        restart_mosquitto_if_required()
-
-    except Exception:
-        logger.exception(
-            "Unable to configure Mosquitto login"
-        )
-        raise
 
     mqttc = Comfort2(
         callback_api_version=mqtt.CallbackAPIVersion.VERSION2,
